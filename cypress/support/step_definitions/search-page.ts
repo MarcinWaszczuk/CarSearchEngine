cdc4d0b7adc8106cc25677baa74b8cb7aab3d24b
MCWZ <mrw@spyro-soft.com>
2023-03-09 16:55:11 +0100

    prettier test 2

--- cypress/support/step_definitions/search-page.ts
+++ cypress/support/step_definitions/search-page.ts
@@ -1,8 +1,4 @@
-import {
-  Given,
-  When,
-  Then,
-} from '@badeball/cypress-cucumber-preprocessor'
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 import SearchPage from '../../page_objects/searchPage'
 const search = new SearchPage()
 
@@ -10,24 +6,18 @@ Given('I visit homepage', () => {
   search.visit()
 })
 
-When(
-  'I filter cars list by {string} model filter using valid dates',
-  (model: string) => {
-    search.fillModel(model)
-    search.fillPickupDate('2023-05-05')
-    search.fillDropOffDate('2023-06-05')
-    search.clickSearchButton()
-  },
-)
+When('I filter cars list by {string} model filter using valid dates', (model: string) => {
+  search.fillModel(model)
+  search.fillPickupDate('2023-05-05')
+  search.fillDropOffDate('2023-06-05')
+  search.clickSearchButton()
+})
 
-When(
-  'I filter cars list using pickup date later than drop off date',
-  () => {
-    search.fillFuturePickupDateByDaysFromNow(15)
-    search.fillFutureDropOffDateByDaysFromNow(10)
-    search.clickSearchButton()
-  },
-)
+When('I filter cars list using pickup date later than drop off date', () => {
+  search.fillFuturePickupDateByDaysFromNow(15)
+  search.fillFutureDropOffDateByDaysFromNow(10)
+  search.clickSearchButton()
+})
 
 When('I filter cars list using valid dates', () => {
   search.fillFuturePickupDateByDaysFromNow(5)
@@ -35,21 +25,15 @@ When('I filter cars list using valid dates', () => {
   search.clickSearchButton()
 })
 
-Then(
-  'I should see the table with only particular {string} cars',
-  (model: string) => {
-    cy.get('tbody > tr').each($el => {
-      expect($el).to.contain(model)
-    })
-  },
-)
+Then('I should see the table with only particular {string} cars', (model: string) => {
+  cy.get('tbody > tr').each($el => {
+    expect($el).to.contain(model)
+  })
+})
 
-Then(
-  'I should see the alert saying {string}',
-  (alert: string) => {
-    cy.get('.alert').should('contain', alert)
-  },
-)
+Then('I should see the alert saying {string}', (alert: string) => {
+  cy.get('.alert').should('contain', alert)
+})
 
 Then('I should see the table of cars', () => {
   cy.get('#search-results').should('be.visible')
